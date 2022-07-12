0ff3e01708cae0bd702727b3739968138b6e3022
Sam Bugs <sambugs@protonmail.com>
2022-07-11 23:13:16 -0300

    chore: add docs

--- solidity/contracts/SwapAdapter.sol
+++ solidity/contracts/SwapAdapter.sol
@@ -16,10 +16,22 @@ abstract contract SwapAdapter is ISwapAdapter {
     SWAPPER_REGISTRY = ISwapperRegistry(_swapperRegistry);
   }
 
+  /**
+   * @notice Takes the given amount of tokens from the caller
+   * @param _token The token to check
+   * @param _amount The amount to take
+   */
   function _takeFromMsgSender(IERC20 _token, uint256 _amount) internal virtual {
     _token.safeTransferFrom(msg.sender, address(this), _amount);
   }
 
+  /**
+   * @notice Checks if the given spender has enough allowance, and approves the max amount
+   *         if it doesn't
+   * @param _token The token to check
+   * @param _spender The spender to check
+   * @param _minAllowance The min allowance. If the spender has over this amount, then no extra approve is needed
+   */
   function _maxApproveSpenderIfNeeded(
     IERC20 _token,
     address _spender,
@@ -34,10 +46,20 @@ abstract contract SwapAdapter is ISwapAdapter {
     }
   }
 
+  /**
+   * @notice Executes a swap for the given swapper
+   * @param _swapper The actual swapper
+   * @param _swapData The swap execution
+   */
   function _executeSwap(address _swapper, bytes calldata _swapData) internal virtual {
     _swapper.functionCallWithValue(_swapData, msg.value);
   }
 
+  /**
+   * @notice Checks if the contract has any balance of the given token, and if it does,
+   *         it sends it to the caller
+   * @param _token The token to check
+   */
   function _sendBalanceToMsgSender(IERC20 _token) internal virtual {
     uint256 _balance = _token.balanceOf(address(this));
     if (_balance > 0) {
@@ -45,6 +67,10 @@ abstract contract SwapAdapter is ISwapAdapter {
     }
   }
 
+  /**
+   * @notice Checks if given swapper is allowlisted, and fails if it isn't
+   * @param _swapper The swapper to check
+   */
   function _assertSwapperIsAllowlisted(address _swapper) internal view {
     if (!SWAPPER_REGISTRY.isAllowlisted(_swapper)) revert SwapperNotAllowlisted(_swapper);
   }
