28a046807e0dd2ddfb7fae09ac36852a00370d66
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2022-07-12 12:38:15 -0300

    feat: add take from sender (#13)

--- test/unit/swap-adapter.spec.ts
+++ test/unit/swap-adapter.spec.ts
@@ -1,27 +1,38 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { constants } from 'ethers';
 import { behaviours } from '@utils';
-import { then, when } from '@utils/bdd';
-import { ISwapperRegistry, SwapAdapterMock, SwapAdapterMock__factory } from '@typechained';
+import { given, then, when } from '@utils/bdd';
+import { IERC20, ISwapperRegistry, SwapAdapterMock, SwapAdapterMock__factory } from '@typechained';
 import { snapshot } from '@utils/evm';
 import { FakeContract, smock } from '@defi-wonderland/smock';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+chai.use(smock.matchers);
 
 describe('SwapAdapter', () => {
+  const AMOUNT = 1000000;
+
+  let caller: SignerWithAddress;
   let swapAdapterFactory: SwapAdapterMock__factory;
   let swapAdapter: SwapAdapterMock;
   let registry: FakeContract<ISwapperRegistry>;
   let snapshotId: string;
+  let token: FakeContract<IERC20>;
 
   before('Setup accounts and contracts', async () => {
+    [caller] = await ethers.getSigners();
     registry = await smock.fake('ISwapperRegistry');
     swapAdapterFactory = await ethers.getContractFactory('solidity/contracts/test/SwapAdapter.sol:SwapAdapterMock');
     swapAdapter = await swapAdapterFactory.deploy(registry.address);
+    token = await smock.fake('IERC20');
     snapshotId = await snapshot.take();
   });
 
   beforeEach(async () => {
     await snapshot.revert(snapshotId);
+    token.transferFrom.reset();
+    token.transferFrom.returns(true);
   });
 
   describe('constructor', () => {
@@ -37,6 +48,17 @@ describe('SwapAdapter', () => {
     when('all arguments are valid', () => {
       then('registry is set correctly', async () => {
         expect(await swapAdapter.SWAPPER_REGISTRY()).to.equal(registry.address);
+      });
+    });
+  });
+
+  describe('_takeFromMsgSender', () => {
+    when('function is called', () => {
+      given(async () => {
+        await swapAdapter.internalTakeFromMsgSender(token.address, AMOUNT);
+      });
+      then('token is called correctly', async () => {
+        expect(token.transferFrom).to.have.been.calledOnceWith(caller.address, swapAdapter.address, AMOUNT);
       });
     });
   });
