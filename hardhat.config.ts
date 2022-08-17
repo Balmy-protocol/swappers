376ca88c00444b45eaf64f90dc4d0c7b19839676
Sam Bugs <sambugs@protonmail.com>
2022-08-17 15:22:27 -0300

    chore: add bug bounty, update dependencies and increase solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -90,7 +90,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.14',
+        version: '0.8.16',
         settings: {
           optimizer: {
             enabled: true,
